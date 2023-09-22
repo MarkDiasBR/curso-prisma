925dbe34ffb5aa61a8b07982115141e47490ca71
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-09-21 23:09:07 -0300

    aula-prisma-04

---
+++ aula-prisma-04/src/repositories/post-repository.ts
@@ -0,0 +1,42 @@
+import { Post } from "@prisma/client";
+import prisma from "../database/database";
+
+
+const TABLE_NAME = "posts";
+
+export type CreatePost = Omit<Post, "id">
+
+async function getPosts() {
+  const result = await prisma.post.findMany();
+  return result;
+}
+
+async function getPost(id: number) {
+  const post = await prisma.post.findUnique({
+    where: { id }
+  });
+  return post;
+}
+
+
+
+async function createPost(post: CreatePost) {
+  await prisma.post.create({
+    data: post
+  });
+}
+
+async function deletePost(id: number) {
+  await prisma.post.delete({
+    where: { id }
+  })
+}
+
+const postRepository = {
+  getPost,
+  getPosts,
+  createPost,
+  deletePost
+}
+
+export default postRepository;
