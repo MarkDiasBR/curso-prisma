a2bbf607649f83744779da0b82b8bedd1ceb0225
MarkDiasBR <marcelo.dias@ufba.br>
2023-09-28 05:29:34 -0300

    aula-prisma-09

---
+++ aula-prisma-09/prisma/seed.ts
@@ -0,0 +1,60 @@
+import prisma from "../src/database";
+
+async function getDefaultUser() {
+  const user = await prisma.customer.findFirst({
+    where: {
+        AND: [
+            { 
+                document: "133.245.497-60"
+            },
+            {   
+                firstName: {
+                    startsWith: "Geraldo Luiz"
+                }
+            },
+            {
+                lastName: {
+                    endsWith: "Datena"
+                }
+            }
+        ]   
+    }
+  });
+
+  return user;
+}
+
+async function createDefaultUser() {
+  console.log("Creating default user!");
+  const user = await prisma.customer.create({
+    data: {
+        document: "133.245.497-60",
+        firstName: "Geraldo Luiz",
+        lastName: "Datena"
+    }
+  });
+
+  return user;
+}
+
+async function checkOrCreateDefaultUser() {
+  const user = await getDefaultUser();
+  if (!user) await createDefaultUser();
+  else console.log("Default user already created.");
+
+  return user;
+}
+
+async function main() {
+  return checkOrCreateDefaultUser();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
